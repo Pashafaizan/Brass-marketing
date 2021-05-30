b80193b0a1e76ace0d02cd76d77c1810c2f106dc
Mohd Suhail <thesuhailansari786246@gmail.com>
2021-05-30 22:38:47 +0530

    carousel fixed

--- frontend/src/components/CrouselPage/CrouselPage.js
+++ frontend/src/components/CrouselPage/CrouselPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./CrouselPage.css";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
@@ -8,21 +8,44 @@ const CarouselPage = () => {
     "https://images-eu.ssl-images-amazon.com/images/G/31/img21/HPC/GW/Grocery_1500x600._CB669573043_.jpg",
     " https://images-eu.ssl-images-amazon.com/images/G/31/img21/CEPC/Essentials/D23636912_IN_CEPC_BAU-essentals-graphics_May21_1500x300_2.jpg",
     "https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/GW/milaap/Milaap_Hero_pc1x._CB669734096_.jpg",
+    "https://images-eu.ssl-images-amazon.com/images/G/31/Gateway/Zeitgeist/Mar20/Covid19/2021/IN_GWD_Covid19_CustomerMsg_MH_ENG_1x_v1._CB669806110_.jpg",
   ];
   const [imgSlidePos, setImgSlidePos] = useState(0);
+  const [transition, setTransition] = useState("ease-in 1s");
+  const [animationTime, setAnimationTime] = useState(3000);
+  const [disableButton, setDisableButton] = useState(false);
+  
   function goRight() {
-    console.log(imgSlidePos);
-    imgSlidePos === -100 * (slideCrouselImage.length - 1)
-      ? setImgSlidePos(0)
-      : setImgSlidePos(imgSlidePos - 100);
+    console.log(imgSlidePos,"S");
+    
+    if (imgSlidePos === -100 * (slideCrouselImage.length - 1))
+    { 
+      setAnimationTime(0)
+      setTransition("none")
+      setImgSlidePos(0);
+    }
+    else {
+      setAnimationTime(3000)
+      setImgSlidePos(imgSlidePos - 100);
+      setTransition("ease-in 1s")
+    }
   }
 
   function goLeft() {
     console.log(imgSlidePos);
-    imgSlidePos === 0
-      ? setImgSlidePos(-100 * (slideCrouselImage.length - 1))
-      : setImgSlidePos(imgSlidePos + 100);
+    if (imgSlidePos === 0) {
+      setImgSlidePos(-100 * (slideCrouselImage.length - 1))
+    }
+    else {
+
+      setImgSlidePos(imgSlidePos + 100);
+    } 
   }
+
+  useEffect(()=>{
+    console.log("right")
+    window.setTimeout(()=>{goRight();},animationTime)
+  },[imgSlidePos])
 
   // setInterval(function () {
 
@@ -46,19 +69,14 @@ const CarouselPage = () => {
           <div
             key={index}
             className="slide"
-            style={{ transform: `translateX(${imgSlidePos}%)` }}
+            style={{ transform: `translateX(${imgSlidePos}%)`,transition: transition }}
           >
             <img className="home_image" src={item} />
           </div>
         );
       })}
 
-      <button className="go-left" onClick={goLeft}>
-        <ArrowBackIosIcon />
-      </button>
-      <button className="go-right" onClick={goRight}>
-       <ChevronRightIcon/>
-      </button>
+     
     </div>
   );
 };
